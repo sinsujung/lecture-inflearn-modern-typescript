00a1eb2d61316d6f314130234311e6afa919bcff
sinsujung <jhw02045@naver.com>
2025-01-20 17:04:12 +0900

    원시 타입과 리터럴 타입

---
+++ section2/dist/chapter1.js
@@ -0,0 +1,15 @@
+//number
+let num1 = 123;
+let num2 = -123;
+let num3 = 0.123;
+let num4 = -0.123;
+let num5 = Infinity;
+let num6 = -Infinity;
+let num7 = NaN;
+console.log(num3.toFixed(3));
+// string
+let str1 = "hello";
+let str2 = "hello";
+let str3 = `hello`;
+let str4 = `hello ${num1}`;
+export {};
---
+++ section2/src/chapter1.ts
@@ -0,0 +1,39 @@
+//number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+console.log(num3.toFixed(3));
+
+// string
+let str1: string = "hello";
+let str2: string = "hello";
+let str3: string = `hello`;
+let str4: string = `hello ${num1}`;
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// strictNullCheck 가 true면 아래 오류 , false면 오류 x
+// let numA: number = null;
+
+// 리터럴 타입
+let numA: 10 = 10;
+// num = 12;
+
+let strA: "hello" = "hello";
+// strA = "df";
+
+// let boolA : true = false;
+
